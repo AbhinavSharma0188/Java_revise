8e17ceeed65064fbb785da2fe2e638d4bea789c8
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-02-03 16:08:27 +0530

    .

---
+++ OOPS/Student.java
@@ -0,0 +1,10 @@
+package OOPS;
+
+public class Student {
+    String name;
+    int age;
+
+    public void printDetails() {
+        System.out.println("My name is " + name + " and age is " + age);
+    }
+}
--- OOPS/another.java
+++ OOPS/another.java
@@ -1,9 +1,10 @@
 package OOPS;
 
 public class another {
-    Student s=new Student();
-    s.name="milk"
-    s.age=12;
-
-    
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "milk";
+        s.age = 12;
+        s.printDetails();
+    }
 }
