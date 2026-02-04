2def6287c9023d218e5c33ef15c8c4e4e9a406be
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-02-04 14:58:49 +0530

    private access modifier

--- OOPS/Car.java
+++ OOPS/Car.java
@@ -1,12 +1,18 @@
 package OOPS;
 public class Car {
-    String color;
-    int price;
-    int speed;
+   private String color;
+    private int price;
+    private int speed;
     public Car(String color,int price,int speed){
         this.color=color;
         this.price=price;
         this.speed=speed;
+
+    }
+    public void displayCar(){
+        System.out.println(color);
+        System.out.println(price);
+        System.out.println(speed);
 
     }
     
--- OOPS/anotherCar.java
+++ OOPS/anotherCar.java
@@ -2,10 +2,11 @@ package OOPS;
 public class anotherCar {
     public static void main(String[] args) {
 
-        Car c1 = new Car("Red", 1200000, 180);
+        Car c1 = new Car("Red", 1800000, 180);
+        Car c2 = new Car("black", 1900000, 130);
 
-        System.out.println(c1.color);
-        System.out.println(c1.price);
-        System.out.println(c1.speed);
+        c1.displayCar();
+        c2.displayCar();
+        c1.price=
     }
 }
