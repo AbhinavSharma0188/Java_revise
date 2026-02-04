2f4ccf4ca76e44eb9269e065de4dc626f6376dae
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-02-04 15:15:44 +0530

    encapsulation done

--- OOPS/Car.java
+++ OOPS/Car.java
@@ -1,19 +1,23 @@
 package OOPS;
 public class Car {
    private String color;
-    private int price;
-    private int speed;
+     int price;
+     int speed;
     public Car(String color,int price,int speed){
         this.color=color;
         this.price=price;
         this.speed=speed;
 
     }
+     public void setColor(String color){
+        this.color=color;
+    }
+    
     public void displayCar(){
         System.out.println(color);
         System.out.println(price);
         System.out.println(speed);
 
     }
-    
+   
 }
--- OOPS/anotherCar.java
+++ OOPS/anotherCar.java
@@ -5,8 +5,9 @@ public class anotherCar {
         Car c1 = new Car("Red", 1800000, 180);
         Car c2 = new Car("black", 1900000, 130);
 
-        c1.displayCar();
+      
         c2.displayCar();
-        c1.price=
+        c1.setColor("pink");
+          c1.displayCar();
     }
 }
