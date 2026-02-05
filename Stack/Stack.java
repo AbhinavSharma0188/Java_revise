a9cf686f69fd4a742af693d6da8ecf16f04be49a
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-02-05 18:47:52 +0530

    stack started

---
+++ Stack/Stack.java
@@ -0,0 +1,15 @@
+package Stack;
+
+public class Stack {
+    private int arr[];
+    public Stack(){
+        arr=new int [5];
+
+
+    }
+    public Stack(int n){
+        arr=new int[n];
+
+    }
+    
+}
---
+++ Stack/Stack_client.java
@@ -0,0 +1,8 @@
+package Stack;
+
+public class Stack_client {
+    public static void main(String[] args) {
+        Stack st=new Stack();
+    }
+    
+}
