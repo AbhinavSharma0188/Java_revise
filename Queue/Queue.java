fb8854bd96003841c51300e7eda7036a6826c1b9
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-02-06 09:37:49 +0530

    empty full and enque implementation done

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -1,5 +1,28 @@
 package Queue;
 
-public  {
+public class Queue {
+    private int arr[];
+    private int front=0;
+    private int size=arr.length-1;
+    public Queue(){
+        arr=new int[5];
+
+
+    }
+    public  Queue(int n){
+        arr=new int[n];
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public boolean isFull(){
+        return size==arr.length;
+    }
+    public void enque(int element){
+        arr[size]=element;
+        size++;
+       
+    }
+
     
 }
---
+++ Queue/Queue_client.java
@@ -0,0 +1,6 @@
+package Queue;
+
+public class Queue_client {
+    Queue q=new Queue();
+    
+}
