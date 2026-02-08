2a4e5f504f11694b318999de5e918e2eb54f0298
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-02-08 17:39:29 +0530

    started with linked list implementation

---
+++ Linked_List/Linked_List.java
@@ -0,0 +1,15 @@
+package Linked_List;
+
+public class Linked_List {
+    public class Node{
+        int val;
+        Node next;
+
+
+    }
+    private Node head;
+    private int size;
+    private int tail;
+    
+    
+}
