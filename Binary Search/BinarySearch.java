d40b78be3f6f8cf4c5fb1a4f74bc6c5dbc257d65
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-02-26 23:56:01 +0530

    bs

---
+++ Binary Search/BinarySearch.java
@@ -0,0 +1,27 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int target=4;
+
+        int si=0;
+        int ei=arr.length-1;
+        while(si<=ei){
+            int mid=(si+ei)/2;
+            if(arr[mid]==target){
+                System.out.println("Program started");
+               System.out.println(mid);
+               return;
+            }
+            else if(arr[mid]>target){
+                ei=mid-1;
+
+            }
+            else{
+                si=mid+1;
+
+            }
+        }
+    }
+
+    
+}
