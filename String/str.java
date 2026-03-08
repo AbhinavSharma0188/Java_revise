44258987518e1828a3f0acbcfa065f69a7714e7d
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-08 23:40:18 +0530

    countlastOne

--- String/str.java
+++ String/str.java
@@ -1,19 +1,17 @@
-package String;
-
 public class str {
     public static void main(String[] args) {
-        String s="hello";
-        String s1=new String("hello");
-        String s2="hello";
-        String s3=new String("hello");
-        System.out.println(s==s1);
-        System.out.println(s==s2);
-        System.out.println(s==s3);
+        String s = "hello";
+        String s1 = new String("hello");
+        String s2 = "hello";
+        String s3 = new String("hello");
+        System.out.println(s.charAt(3));
+        System.out.println(s == s1);
+        System.out.println(s == s2);
+        System.out.println(s == s3);
         System.out.println(s.equals(s1));
         System.out.println(s.equals(s2));
         System.out.println(s.equals(s3));
-        
+
     }
 
-    
 }
