13f15294c4a74c8f190a7cec265d08289b64b5b6
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-12 12:43:51 +0530

    add first

--- Linked_List/Linked_List.java
+++ Linked_List/Linked_List.java
@@ -10,20 +10,39 @@ public class Linked_List {
     private int size;
     private Node tail;
     public  void addFirst(int item){
-        if(size==0){
-            Node nn=new Node();
+         Node nn=new Node();
             nn.val=item;
+        if(size==0){
+           
             nn.next=null;
             head=nn;
             tail=nn;
+size++;
             
 
+        }
+        else{
+            nn.next=head;
+            head=nn;
+size++;
+        }
+    }
+    public void Display(){
+        Node temp=head;
+
+        while(temp!=null){
+            System.out.println(temp.val);
+            temp=temp.next;
         }
     }
     public static void main(String[] args) {
         Linked_List l1=new Linked_List();
         l1.addFirst(10);
-        System.out.println(l1.head.val);
+        l1.addFirst(20);
+        l1.addFirst(30);
+        l1.Display();
+        
+        
 
 
         
