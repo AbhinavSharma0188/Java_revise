d5942b1079a0847038f249323ee4e2761cc7b117
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-13 13:57:23 +0530

    leetcode 876

---
+++ Linked_List/LEETCODE_876.java
@@ -0,0 +1,26 @@
+import org.w3c.dom.Node;
+
+public class LEETCODE_876 {
+    public class ListNode {
+    int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+   ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;   
+
+        
+    }
+
+    
+}
