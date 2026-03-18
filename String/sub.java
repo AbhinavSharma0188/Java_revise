0a3e18fd8f285cda9cabc11df39800de598ce7e1
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-18 12:34:37 +0530

    sub

---
+++ String/sub.java
@@ -0,0 +1,16 @@
+public class sub {
+    public static void main(String[] args) {
+        String s="abc";
+        
+        printSub(s,"");
+    }
+    public static void printSub(String s,String ans){
+        if(s.length() == 0){
+            System.out.print(ans+" ");
+            return;
+        }
+        char ch=s.charAt(0);
+        printSub(s.substring(1),ans);
+        printSub(s.substring(1),ans+ch);
+    }
+}
