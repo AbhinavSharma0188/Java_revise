7ccd9d6f6b9650fed8fada69603a136d07d431cd
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-22 19:39:43 +0530

    leetcode 6

---
+++ leetcode6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) return s;
+        
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) rows[i] = new StringBuilder();
+        
+        int curr = 0;
+        boolean down = true;
+        
+        for (char c : s.toCharArray()) {
+            rows[curr].append(c);
+            if (curr == 0) down = true;
+            else if (curr == numRows - 1) down = false;
+            curr += down ? 1 : -1;
+        }
+        
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) result.append(row);
+        
+        return result.toString();
+    }
+}
