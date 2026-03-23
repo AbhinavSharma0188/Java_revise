fbc1a1d2bf2f3c174bc9621e1ca72745690d5cd0
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-23 17:33:15 +0530

    leetcode 47

---
+++ leetcode47.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), new boolean[nums.length], res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, List<Integer> temp, boolean[] used, List<List<Integer>> res) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+            used[i] = true;
+            temp.add(nums[i]);
+            backtrack(nums, temp, used, res);
+            temp.remove(temp.size() - 1);
+            used[i] = false;
+        }
+    }
+}
