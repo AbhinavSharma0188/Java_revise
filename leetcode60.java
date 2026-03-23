ccb43cf981211109a8cb7c97f734c579a9fba11c
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-23 17:43:47 +0530

    leetcode 60

---
+++ leetcode60.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> nums = new ArrayList<>();
+        int[] fact = new int[n];
+        fact[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            fact[i] = fact[i - 1] * i;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            nums.add(i);
+        }
+
+        k--;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = n; i > 0; i--) {
+            int index = k / fact[i - 1];
+            sb.append(nums.get(index));
+            nums.remove(index);
+            k %= fact[i - 1];
+        }
+
+        return sb.toString();
+    }
+}
