ce06c449b8b43a116f7558e29dfb6bb5a12b5736
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-24 16:05:29 +0530

    leetcode 71

---
+++ leetcode71.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String simplifyPath(String path) {
+        String[] parts = path.split("/");
+        Stack<String> stack = new Stack<>();
+
+        for (String part : parts) {
+            if (part.equals("") || part.equals(".")) continue;
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(part);
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (String dir : stack) {
+            res.append("/").append(dir);
+        }
+
+        return res.length() == 0 ? "/" : res.toString();
+    }
+}
