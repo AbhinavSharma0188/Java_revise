c9f150edb3a428ed7ec2686362cbee6f681361b4
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-24 16:07:56 +0530

    leetcode 76

---
+++ leetcode76.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+
+        int[] map = new int[128];
+        for (char c : t.toCharArray()) map[c]++;
+
+        int left = 0, count = t.length(), minLen = Integer.MAX_VALUE, start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            if (map[s.charAt(right)]-- > 0) count--;
+
+            while (count == 0) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+                if (map[s.charAt(left)]++ == 0) count++;
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+}
