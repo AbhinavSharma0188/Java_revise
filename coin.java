fbe9775ac6165fe8e1ac96a3db197e6f7135149e
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-30 19:16:15 +0530

    coin combination

--- coin.java
+++ coin.java
@@ -2,9 +2,9 @@ public class coin {
     public static void main(String[] args) {
         int arr[]={2,1,3,5};
         int amount=4;
-        coinn(arr,amount,"");
+        coinn(arr,amount,"",0);
     }
-    public static void coinn(int arr[],int amount,String ans){
+    public static void coinn(int arr[],int amount,String ans,int idx){
         if(amount<0){
             return;
 
@@ -13,10 +13,10 @@ public class coin {
             System.out.println(ans);
             return;
         }
-        for(int i=0;i<=arr.length-1;i++){
+        for(int i=idx;i<=arr.length-1;i++){
         if(amount>=arr[i]){
 
-            coinn(arr, amount-arr[i], ans+arr[i]);
+            coinn(arr, amount-arr[i], ans+arr[i],i);
         
         }
     }
