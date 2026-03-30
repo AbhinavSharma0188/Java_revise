1104dffffcdf0a0c33dc80f2b0e0b2d57c5816fa
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-03-30 11:04:39 +0530

    queen Placed

---
+++ queenPlacedSOFar.java
@@ -0,0 +1,26 @@
+public class queenPlacedSOFar {
+    public static void main(String[] args) {
+        int n=4;
+        boolean arr[]=new boolean[n];
+        int tq=2;
+        permutations(arr,tq,0,"");
+    }
+    public static void permutations(boolean arr[],int tq,int qpsf,String ans){
+        if(qpsf==tq){
+            System.out.println(ans);
+            return;
+        }
+        
+
+
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]==false){
+                arr[i]=true;
+                permutations(arr, tq, qpsf+1, ans+"b"+i+"q"+qpsf);
+                arr[i]=false;
+            }
+        }
+    }
+
+    
+}
