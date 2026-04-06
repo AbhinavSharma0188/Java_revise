11663af3fb7ce2c45a3cc26f3beeabb8de0c952d
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-06 11:50:54 +0530

    removeKthDigits

---
+++ removeKdigits.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : num.toCharArray()) {
+            while (!stack.isEmpty() && k > 0 && stack.peekLast() > c) {
+                stack.pollLast();
+                k--;
+            }
+            stack.offerLast(c);
+        }
+
+        while (k > 0) {
+            stack.pollLast();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean leadingZero = true;
+
+        for (char c : stack) {
+            if (leadingZero && c == '0') continue;
+            leadingZero = false;
+            sb.append(c);
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
