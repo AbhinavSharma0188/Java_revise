14d44b3c5011c97e7c82aec4410c07292e1f42d7
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-07 12:00:43 +0530

    447

---
+++ 447.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length;
+        Map<Long, Integer>[] dp = new HashMap[n];
+        for (int i = 0; i < n; i++) dp[i] = new HashMap<>();
+        
+        int res = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                long diff = (long) nums[i] - nums[j];
+                int count = dp[j].getOrDefault(diff, 0);
+                res += count;
+                dp[i].put(diff, dp[i].getOrDefault(diff, 0) + count + 1);
+            }
+        }
+        
+        return res;
+    }
+}
