4f1de24490e41cd2f59c76c7b2795277db3fa15f
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-11 22:17:09 +0530

    496

---
+++ 496.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        java.util.Map<Integer, Integer> map = new java.util.HashMap<>();
+        java.util.Stack<Integer> stack = new java.util.Stack<>();
+        
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        
+        return res;
+    }
+}
