3f8b42dbd9719e1b650b859fcc8a609d7221f647
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-12 19:13:58 +0530

    433

---
+++ 433.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        Set<String> set = new HashSet<>(Arrays.asList(bank));
+        if (!set.contains(endGene)) return -1;
+        
+        char[] genes = {'A', 'C', 'G', 'T'};
+        Queue<String> q = new LinkedList<>();
+        q.offer(startGene);
+        
+        Set<String> visited = new HashSet<>();
+        visited.add(startGene);
+        
+        int steps = 0;
+        
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                String curr = q.poll();
+                if (curr.equals(endGene)) return steps;
+                
+                char[] arr = curr.toCharArray();
+                for (int j = 0; j < arr.length; j++) {
+                    char old = arr[j];
+                    for (char c : genes) {
+                        arr[j] = c;
+                        String next = new String(arr);
+                        if (set.contains(next) && !visited.contains(next)) {
+                            visited.add(next);
+                            q.offer(next);
+                        }
+                    }
+                    arr[j] = old;
+                }
+            }
+            steps++;
+        }
+        
+        return -1;
+    }
+}
