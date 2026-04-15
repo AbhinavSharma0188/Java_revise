a6cb5e24ab823b96c9d7f15d4554f0090ece5ac6
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-15 21:02:40 +0530

    466'

--- 464.java
+++ 464.java
@@ -1,24 +1,43 @@
 class Solution {
-    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-        if (desiredTotal <= 0) return true;
-        int sum = (maxChoosableInteger * (maxChoosableInteger + 1)) / 2;
-        if (sum < desiredTotal) return false;
-        return dfs(desiredTotal, 0, maxChoosableInteger, new Boolean[1 << maxChoosableInteger]);
-    }
+    public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+        if (n1 == 0) return 0;
 
-    private boolean dfs(int total, int used, int max, Boolean[] dp) {
-        if (total <= 0) return false;
-        if (dp[used] != null) return dp[used];
+        int s1Count = 0, s2Count = 0, index = 0;
+        int[] recall = new int[s2.length() + 1];
+        int[] count = new int[s2.length() + 1];
 
-        for (int i = 1; i <= max; i++) {
-            int mask = 1 << (i - 1);
-            if ((used & mask) == 0) {
-                if (!dfs(total - i, used | mask, max, dp)) {
-                    return dp[used] = true;
+        while (true) {
+            s1Count++;
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1.charAt(i) == s2.charAt(index)) {
+                    index++;
+                    if (index == s2.length()) {
+                        s2Count++;
+                        index = 0;
+                    }
                 }
             }
-        }
 
-        return dp[used] = false;
+            if (s1Count == n1) {
+                return s2Count / n2;
+            }
+
+            if (recall[index] != 0) {
+                int prevS1Count = recall[index];
+                int prevS2Count = count[index];
+
+                int cycleS1 = s1Count - prevS1Count;
+                int cycleS2 = s2Count - prevS2Count;
+
+                int remaining = n1 - s1Count;
+                int times = remaining / cycleS1;
+
+                s1Count += times * cycleS1;
+                s2Count += times * cycleS2;
+            }
+
+            recall[index] = s1Count;
+            count[index] = s2Count;
+        }
     }
 }
