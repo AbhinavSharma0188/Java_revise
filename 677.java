39e50aff06bfc78aecd343c49c6d25c28d58cf59
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-22 18:06:54 +0530

    677

---
+++ 677.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class MapSum {
+    HashMap<String, Integer> map;
+
+    public MapSum() {
+        map = new HashMap<>();
+    }
+
+    public void insert(String key, int val) {
+        map.put(key, val);
+    }
+
+    public int sum(String prefix) {
+        int res = 0;
+        for (String key : map.keySet()) {
+            if (key.startsWith(prefix)) {
+                res += map.get(key);
+            }
+        }
+        return res;
+    }
+}
