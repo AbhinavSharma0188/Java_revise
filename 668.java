d23cd2a4d5266003419dfa2e0762058ad7c384c4
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-22 17:58:44 +0530

    668

---
+++ 668.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
+        int low = 1, high = m * n;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (count(mid, m, n) >= k) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private int count(int x, int m, int n) {
+        int cnt = 0;
+        for (int i = 1; i <= m; i++) {
+            cnt += Math.min(x / i, n);
+        }
+        return cnt;
+    }
+}
