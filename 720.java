2f4da3cc52cf7c689b4d5cbf77e548c64e3c62da
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-23 14:45:03 +0530

    720

---
+++ 720.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+        Set<String> set = new HashSet<>();
+        String res = "";
+
+        for (String word : words) {
+            if (word.length() == 1 || set.contains(word.substring(0, word.length() - 1))) {
+                if (word.length() > res.length()) {
+                    res = word;
+                }
+                set.add(word);
+            }
+        }
+
+        return res;
+    }
+}
