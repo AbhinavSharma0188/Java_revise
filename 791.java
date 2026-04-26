4a8a5d88cf12dc8a4fc88d3c584356ee39fc62f4
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-26 21:21:08 +0530

    791

---
+++ 791.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        int[] freq = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : order.toCharArray()) {
+            while (freq[c - 'a']-- > 0) {
+                sb.append(c);
+            }
+        }
+        
+        for (char c = 'a'; c <= 'z'; c++) {
+            while (freq[c - 'a']-- > 0) {
+                sb.append(c);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
