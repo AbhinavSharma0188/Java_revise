20295c35f46b106f450322580bbbba4a1791ec74
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-28 18:32:08 +0530

    826

---
+++ 826.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int n = difficulty.length;
+        int[][] jobs = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            jobs[i][0] = difficulty[i];
+            jobs[i][1] = profit[i];
+        }
+
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        Arrays.sort(worker);
+
+        int i = 0, maxProfit = 0, res = 0;
+
+        for (int w : worker) {
+            while (i < n && jobs[i][0] <= w) {
+                maxProfit = Math.max(maxProfit, jobs[i][1]);
+                i++;
+            }
+            res += maxProfit;
+        }
+
+        return res;
+    }
+}
