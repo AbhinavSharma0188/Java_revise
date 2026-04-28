ed8c31a4d0de4dfbff79ede99418ca533f54e1fa
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-28 17:41:06 +0530

    820

---
+++ 820.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+
+        for (String word : words) {
+            for (int i = 1; i < word.length(); i++) {
+                set.remove(word.substring(i));
+            }
+        }
+
+        int length = 0;
+        for (String word : set) {
+            length += word.length() + 1;
+        }
+
+        return length;
+    }
+}
