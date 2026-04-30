da4a4590c64001071f773cebca759a1d631a0dca
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-04-30 19:49:50 +0530

    866

---
+++ 866.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int primePalindrome(int n) {
+        if (n <= 2) return 2;
+        if (n <= 3) return 3;
+        if (n <= 5) return 5;
+        if (n <= 7) return 7;
+        if (n <= 11) return 11;
+        
+        for (int i = 1; i < 100000; i++) {
+            String s = Integer.toString(i);
+            StringBuilder sb = new StringBuilder(s.substring(0, s.length() - 1)).reverse();
+            int num = Integer.parseInt(s + sb.toString());
+            
+            if (num >= n && isPrime(num)) return num;
+        }
+        
+        return -1;
+    }
+    
+    private boolean isPrime(int x) {
+        if (x < 2) return false;
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+}
