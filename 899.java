ac625a56a03a663336a67586b63a984d2d14cbe8
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-05-02 17:21:09 +0530

    LeetCode 899 - Orderly Queue

---
+++ 899.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String orderlyQueue(String s, int k) {
+        if (k > 1) {
+            char[] arr = s.toCharArray();
+            java.util.Arrays.sort(arr);
+            return new String(arr);
+        }
+
+        String res = s;
+        for (int i = 1; i < s.length(); i++) {
+            String temp = s.substring(i) + s.substring(0, i);
+            if (temp.compareTo(res) < 0) {
+                res = temp;
+            }
+        }
+        return res;
+    }
+}
