12156c3f0a3c449fa7b2297a85a3fbfdb6c83de0
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-05-04 10:21:44 +0530

    Leetcode 915 Partition Array into Disjoint Intervals

---
+++ 915.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int partitionDisjoint(int[] nums) {
+        int n = nums.length;
+        int[] rightMin = new int[n];
+
+        rightMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMin[i] = Math.min(nums[i], rightMin[i + 1]);
+        }
+
+        int leftMax = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            if (leftMax <= rightMin[i]) return i;
+            leftMax = Math.max(leftMax, nums[i]);
+        }
+
+        return -1;
+    }
+}
