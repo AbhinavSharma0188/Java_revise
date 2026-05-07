555fbf61478901f951e6b122213aa4aef8e5615b
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-05-07 20:19:27 +0530

    Solved LeetCode 937 Reorder Data in Log Files

---
+++ 937.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        java.util.Arrays.sort(logs, (a, b) -> {
+            String[] split1 = a.split(" ", 2);
+            String[] split2 = b.split(" ", 2);
+
+            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+
+            if (!isDigit1 && !isDigit2) {
+                int cmp = split1[1].compareTo(split2[1]);
+
+                if (cmp != 0) {
+                    return cmp;
+                }
+
+                return split1[0].compareTo(split2[0]);
+            }
+
+            if (!isDigit1) {
+                return -1;
+            }
+
+            if (!isDigit2) {
+                return 1;
+            }
+
+            return 0;
+        });
+
+        return logs;
+    }
+}
