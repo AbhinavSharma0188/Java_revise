bbed9a01aea442c93b271e602b61819702e76ed7
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-05-11 18:33:38 +0530

    Solve LeetCode 957 Prison Cells After N Days

---
+++ 957.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] prisonAfterNDays(int[] cells, int n) {
+        Map<String, Integer> seen = new HashMap<>();
+
+        while (n > 0) {
+            String state = Arrays.toString(cells);
+
+            if (seen.containsKey(state)) {
+                int cycleLength = seen.get(state) - n;
+                n %= cycleLength;
+            }
+
+            seen.put(state, n);
+
+            if (n > 0) {
+                n--;
+                cells = nextDay(cells);
+            }
+        }
+
+        return cells;
+    }
+
+    private int[] nextDay(int[] cells) {
+        int[] next = new int[8];
+
+        for (int i = 1; i < 7; i++) {
+            next[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
+        }
+
+        return next;
+    }
+}
