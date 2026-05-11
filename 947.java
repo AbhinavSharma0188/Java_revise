2898ac7650d34f71f982bc52763cd5f4137feb21
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-05-11 18:25:43 +0530

    Solve LeetCode 947 Most Stones Removed with Same Row or Column

---
+++ 947.java
@@ -0,0 +1,46 @@
+class Solution {
+    int[] parent;
+
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        parent = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        int components = n;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    if (union(i, j)) {
+                        components--;
+                    }
+                }
+            }
+        }
+
+        return n - components;
+    }
+
+    private int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+
+        return parent[x];
+    }
+
+    private boolean union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa == pb) {
+            return false;
+        }
+
+        parent[pa] = pb;
+        return true;
+    }
+}
