972af2c20a054086ba0e53fee33013ecc23e8862
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-05-12 18:57:02 +0530

    Solve LeetCode 965 Univalued Binary Tree

---
+++ 965.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        return dfs(root, root.val);
+    }
+
+    private boolean dfs(TreeNode node, int value) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val != value) {
+            return false;
+        }
+
+        return dfs(node.left, value) && dfs(node.right, value);
+    }
+}
