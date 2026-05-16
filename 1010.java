6e13188088b9889cabe451fb210419e1b7b33c85
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-05-16 12:00:10 +0530

    Solve LeetCode 1010 Pairs of Songs With Total Durations Divisible by 60

---
+++ 1010.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+        int[] freq = new int[60];
+        int count = 0;
+
+        for (int t : time) {
+            int rem = t % 60;
+            int complement = (60 - rem) % 60;
+
+            count += freq[complement];
+            freq[rem]++;
+        }
+
+        return count;
+    }
+}
