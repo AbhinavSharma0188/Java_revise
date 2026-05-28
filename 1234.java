df025d2006bd3de3ce3611972941e90c09ebd519
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-05-28 12:32:09 +0530

    Solved LeetCode 1234 Replace the Substring for Balanced String

---
+++ 1234.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int balancedString(String s) {
+        int n = s.length();
+        int k = n / 4;
+
+        int[] count = new int[128];
+
+        for (char c : s.toCharArray()) {
+            count[c]++;
+        }
+
+        if (count['Q'] == k &&
+            count['W'] == k &&
+            count['E'] == k &&
+            count['R'] == k) {
+            return 0;
+        }
+
+        int left = 0;
+        int ans = n;
+
+        for (int right = 0; right < n; right++) {
+            count[s.charAt(right)]--;
+
+            while (left < n &&
+                   count['Q'] <= k &&
+                   count['W'] <= k &&
+                   count['E'] <= k &&
+                   count['R'] <= k) {
+
+                ans = Math.min(ans, right - left + 1);
+
+                count[s.charAt(left)]++;
+                left++;
+            }
+        }
+
+        return ans;
+    }
+}
