79f8d11f550df5cef5c439198d38be78eb2c29fe
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-06-02 16:37:32 +0530

    Solved LeetCode 1287 - Element Appearing More Than 25% In Sorted Array

---
+++ 1287.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            int j = i;
+
+            while (j < n && arr[j] == arr[i]) {
+                j++;
+            }
+
+            if (j - i > n / 4) {
+                return arr[i];
+            }
+
+            i = j - 1;
+        }
+
+        return -1;
+    }
+}
