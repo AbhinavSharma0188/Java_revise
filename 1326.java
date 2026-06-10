89b83d0f8edec4354306fd37817a1e0aaa4e7b3a
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-06-10 22:36:46 +0530

    Solved LeetCode 1326 - Minimum Number of Taps to Open to Water a Garden

---
+++ 1326.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minTaps(int n, int[] ranges) {
+        int[] maxReach = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            int left = Math.max(0, i - ranges[i]);
+            int right = Math.min(n, i + ranges[i]);
+            maxReach[left] = Math.max(maxReach[left], right);
+        }
+
+        int taps = 0;
+        int currEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i <= n; i++) {
+            if (i > farthest)
+                return -1;
+
+            farthest = Math.max(farthest, maxReach[i]);
+
+            if (i == currEnd) {
+                if (currEnd != n) {
+                    taps++;
+                    currEnd = farthest;
+                }
+            }
+        }
+
+        return taps;
+    }
+}
