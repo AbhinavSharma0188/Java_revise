0a090ba583aaac38608e77e9a94c92bb1a0b1742
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-06-11 22:42:40 +0530

    Solve LeetCode 1331 Rank Transform of an Array

---
+++ 1331.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> rank = new HashMap<>();
+        int r = 1;
+
+        for (int num : sorted) {
+            if (!rank.containsKey(num)) {
+                rank.put(num, r++);
+            }
+        }
+
+        int[] result = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = rank.get(arr[i]);
+        }
+
+        return result;
+    }
+}
