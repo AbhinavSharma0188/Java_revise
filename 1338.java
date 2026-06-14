8e5de4a18d56ad44f5eb94db28413e5e578b570b
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-06-14 21:44:39 +0530

    Solve LeetCode 1338 Reduce Array Size to The Half

---
+++ 1338.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int minSetSize(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> freq = new ArrayList<>(map.values());
+        freq.sort(Collections.reverseOrder());
+
+        int removed = 0;
+        int count = 0;
+        int half = arr.length / 2;
+
+        for (int f : freq) {
+            removed += f;
+            count++;
+
+            if (removed >= half) {
+                return count;
+            }
+        }
+
+        return count;
+    }
+}
