337a8bc83ff550117aca014bb5932026d2c22020
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-07-02 22:31:35 +0530

    Solve LeetCode 1403 - Minimum Subsequence in Non-Increasing Order

---
+++ 1403.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> minSubsequence(int[] nums) {
+        Arrays.sort(nums);
+
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int sum = 0;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            sum += nums[i];
+            result.add(nums[i]);
+
+            if (sum > total - sum) {
+                break;
+            }
+        }
+
+        return result;
+    }
+}
