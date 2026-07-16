19b8a6cb38a5e0243ee40da9ac3a0699cb38bc88
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-07-16 22:52:17 +0530

    Solved LeetCode 1447 - Simplified Fractions

---
+++ 1447.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> simplifiedFractions(int n) {
+        List<String> result = new ArrayList<>();
+
+        for (int denominator = 2; denominator <= n; denominator++) {
+            for (int numerator = 1; numerator < denominator; numerator++) {
+                if (gcd(numerator, denominator) == 1) {
+                    result.add(numerator + "/" + denominator);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
