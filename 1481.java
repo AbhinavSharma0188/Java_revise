094b0929ef2302d293e6a2509f72283706aad521
Abhinav sharma <abhinavsharma3614@gmail.com>
2026-07-30 23:42:59 +0530

    Solved LeetCode 1481 - Least Number of Unique Integers after K Removals

---
+++ 1481.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> freq = new ArrayList<>(map.values());
+        Collections.sort(freq);
+
+        int unique = freq.size();
+
+        for (int f : freq) {
+            if (k >= f) {
+                k -= f;
+                unique--;
+            } else {
+                break;
+            }
+        }
+
+        return unique;
+    }
+}
